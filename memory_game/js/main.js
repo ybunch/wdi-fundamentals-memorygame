4ea0a9e5bc9ea6de863c085164e7e9397efa55a5
Jenz Delmar <wmb@Jenz-Delmars-MacBook-Pro.local>
2019-10-04 22:25:14 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+
+console.log("Up and running!");
+
+var cardOne = "King";
+var cardTwo = "Queen";
+var cardThree = "King";
+var cardFour = "Queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
