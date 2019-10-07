20999d76acd0492ee5eb52e372de9f9afe0ff7b4
Jenz Delmar <wmb@Jenz-Delmars-MacBook-Pro.local>
2019-10-07 16:35:40 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,48 @@
 
-console.log("Up and running!");
+const cards = [ 
 
 
+{
+	rank: "Queen",
+	suit: "Hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
 
 
+{	rank: "Queen",
+	suit: "Diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
 
 
-const cards = ["queen","queen", "king", "king"];
+{	rank: "King",
+	suit: "Hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+
+{
+	rank: "King",
+	suit: "Diamonds",
+	cardImage: "images/king-of-diamonds.png"
+},
+
+];
+
 
 var cardsInPlay = [];
 
+function flipCard (cardId) 
+{ 
+
 var cardOne = cards[0];
 
-cardsInPlay.push(cardOne);
+cardsInPlay.push(cards[cardId].rank);
 
-console.log("User flipped " + cardOne);
+console.log("User flipped " + cards[cardId].rank);
+console.log("User flipped" + cards[cardId].suit);
+console.log("User flipped" + cards[cardId].cardImage);
 
-var cardTwo = cards[0];
 
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
 console.log(cardsInPlay);
 
 if (cardsInPlay.length === 2) {
@@ -32,56 +54,70 @@ if (cardsInPlay.length === 2) {
 		alert("sorry try again");
 	}
 }
+}
 
+flipCard (0)
+
+flipCard (2)
 /*
 
+Instructions
+Next update the cards[cardId] portion of cardsInPlay.push(cards[cardId]);. You'll want to push the name of the card ("queen" or "king") to the cardsInPlay array.
+HINT: You'll want to use the rank property like we did in the last step.
 
-Use console.log() to display the card that the user flipped
+Now let's add a bit of new code so that we can "see" the cards that are flipped in our console. Add two console.log() statements. One should log the cardImage and the other should log the suit for the flipped card.
 
-The message that is displayed in the console should say "User flipped king".
-After saving the changes you made to main.js, open the index.html file in Sublime Text, right click, and open the project in Chrome. Then navigate to the console and check to make sure that this message is displayed. If not, double-check to make sure that you've saved any changes to your main.js file.
-Next we'll add logic to check to see if two cards have been played, because in our final game we will not want to check for a match until the user has selected two cards.
-
-Write an if statement that checks to see if the length of the cardsInPlay array is 2.
-For the condition, you can use the length property to find out how many items are in the cardsInPlay array and then use the === operator to see if the length is equal to 2.
-
-console.log("User flipped " + cardThree);
-
-For testing purposes, it will be helpful to display the card that was just flipped to the console to make sure we've done things correctly.
-
-Use console.log() to display the card that the user flipped
-The message that is displayed in the console should say "User flipped queen".
-After saving the changes you made to main.js, open the index.html file in Sublime Text, right click, and open the project in Chrome. Then navigate to the console in Chrome and check to make sure that this message is displayed. If not, double-check to make sure that you've saved any changes to your main.js file.
-Now create a variable to represent the second card that the user flipped.
-
-Create a variable cardTwo.
-Store the third item in the cards array in the variable cardTwo.
-Now we'll want to add this second card to the cardsInPlay array.
-
-Use the push() method to add cardTwo to the end of the cardsInPlay array.
+Save your main.js file, open the project in Chrome and open the console. Make sure that the "User flipped queen" and "User flipped king" messages are still displayed. You should now also see the image paths and suits displayed.
 
 
-Next up, you'll create a variable to represent the first card that the user flipped.
-Create a variable cardOne.
-Store the first item in the cards array in the variable cardOne.
-HINT: Use the array name, square brackets, and the index number to retrieve this card from the array.
 
-Now we'll want to add this first card to the cardsInPlay array.
-Use the push() method to add cardOne to the end of the cardsInPlay array.
-Here's an example of the push() method if you need a syntax refresher:
-ghosts.push('clyde');
-Open the memory_game folder in Sublime and open the main.js file. You'll be writing code to work through the following steps in Sublime Text.
 
-In your main.js file, create an array to store the four cards.
 
-Create an array cards.
-The array should hold four strings - one for each of our cards.
-The strings should be "queen", "queen", "king", and "king".
-After creating the cards array, you can delete the variables you created in the last unit's project.
-Next we'll want to create an empty array. This will eventually hold the cards that are in play.
 
-Create an array cardsInPlay.
-Set its value to an empty array [] */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+First let's add some extra information to each card by storing an object for each card in our cards array. Open the memory_game folder in Sublime, then open the main.js file you worked on in the last assignment and complete the following steps:
+
+Delete the four strings from the cards array.
+In the cards array, create four objects, one for each card.
+HINT: Here's the syntax for creating an array that contains objects:
+
+var myArray = [
+{
+color: 'red',
+flower: 'rose',
+petals: 20
+},
+{
+color: 'blue',
+flower: 'violet',
+petals: 6
+}
+];
+Here are the values each object should hold:
+rank	suit	cardImage
+"queen"	"hearts"	"images/queen-of-hearts.png"
+"queen"	"diamonds"	"images/queen-of-diamonds.png"
+"king"	"hearts"	"images/king-of-hearts.png"
+"king"	"diamonds"	"images/king-of-diamonds.png"
+
+
+ */
 
 
 /*
